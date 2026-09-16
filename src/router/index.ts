63ff76571c039165d907b50a8a977c468176d18c
ADSwww <2689370433@qq.com>
2026-09-16 19:55:19 +0800

    feat(step3): export roundtable router

---
+++ src/router/index.ts
@@ -0,0 +1,10 @@
+export { RoundtableRouter } from "./roundtable-router.js";
+export type {
+  AgentId,
+  RouteTarget,
+  RoutedAgentEvent,
+  RoundtableAgentAdapter,
+  RoundtableRoomState,
+  RoundtableRouterOptions,
+  RoundtableSendInput,
+} from "./types.js";
