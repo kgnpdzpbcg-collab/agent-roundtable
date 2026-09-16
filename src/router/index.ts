bcf4a8d082bb8a37662b5a6581f98c9ebeccb4ca
ADSwww <2689370433@qq.com>
2026-09-16 20:11:31 +0800

    feat(step4): export discussion mode types

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 export { RoundtableRouter } from "./roundtable-router.js";
 export type {
   AgentId,
+  DiscussionMode,
   RouteTarget,
   RoutedAgentEvent,
   RoundtableAgentAdapter,
   RoundtableRoomState,
   RoundtableRouterOptions,
   RoundtableSendInput,
+  RoundtableStage,
 } from "./types.js";
